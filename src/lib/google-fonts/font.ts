2086fdf5a8798862fd1d2838593ac95b96febb3c
Leonardo Braga <programer_frontbraga@outlook.com>
2025-07-12 22:28:41 -0300

    chore: define fonts and colors

---
+++ src/lib/google-fonts/font.ts
@@ -0,0 +1,18 @@
+import { Alumni_Sans_Pinstripe, Inter, Zain } from 'next/font/google';
+
+const zain = Zain({
+	subsets: ['latin'],
+	weight: ['200', '400', '700', '900'],
+});
+
+const inter = Inter({
+	subsets: ['latin'],
+	weight: ['200', '400', '700', '900'],
+});
+
+const alumni = Alumni_Sans_Pinstripe({
+	subsets: ['latin'],
+	weight: ['400'],
+});
+
+export { alumni, inter, zain };
